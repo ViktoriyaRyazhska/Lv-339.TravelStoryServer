c64d5f520d145c51eae294e6fe2018ad6558bd47
Vova Fuks <vovafuks1996@gmail.com>
2018-09-17 12:26:03 +0300

    added user DAO and user service

---
+++ src/main/java/com/travelstory/dao/UserDAO.java
@@ -0,0 +1,8 @@
+package com.travelstory.dao;
+
+import com.travelstory.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDAO extends JpaRepository<User,Long> {
+
+}
---
+++ src/main/java/com/travelstory/services/UserService.java
@@ -0,0 +1,4 @@
+package com.travelstory.services;
+
+public class UserService {
+}
