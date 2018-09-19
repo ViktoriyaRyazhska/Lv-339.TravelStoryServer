0af27d1fbdfc1e9667b71e15b5c190666c99b71d
Vova Fuks <vovafuks1996@gmail.com>
2018-09-19 13:12:50 +0300

    Added cahnges to token provider

--- src/main/java/com/travelstory/sequrity/TokenProvider.java
+++ src/main/java/com/travelstory/sequrity/TokenProvider.java
@@ -1,10 +1,15 @@
 package com.travelstory.sequrity;
 
 import com.travelstory.entity.UserRole;
+import com.travelstory.services.UserDetailServiceImplementation;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -12,6 +17,7 @@ import java.util.Base64;
 import java.util.Date;
 
 @Component
+@RequiredArgsConstructor
 public class TokenProvider {
 
     @Value("SecretKey")
@@ -20,8 +26,7 @@ public class TokenProvider {
     @Value("259200000")
     private long validityInMilliseconds;
 
-    public TokenProvider() {
-    }
+    private final UserDetailServiceImplementation userDetails;
 
     @PostConstruct
     protected void init() {
@@ -38,4 +43,14 @@ public class TokenProvider {
         return Jwts.builder().setClaims(claims).setIssuedAt(now).setExpiration(validity)
                 .signWith(SignatureAlgorithm.HS256, secretKey).compact();
     }
+
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetail = userDetails.loadUserByUsername(getEmail(token));
+        return new UsernamePasswordAuthenticationToken(userDetail, "", userDetail.getAuthorities());
+    }
+
+    public String getEmail(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
 }
