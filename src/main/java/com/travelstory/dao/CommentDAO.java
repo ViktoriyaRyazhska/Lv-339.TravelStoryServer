00e7090cc8a1f6e137ad11553db4b21b5d56e753
mdzhura <32909428+mdzhura@users.noreply.github.com>
2018-09-27 19:58:56 +0300

    Add realization to AdminServiceImpl and complete AdminController

--- src/main/java/com/travelstory/dao/CommentDAO.java
+++ src/main/java/com/travelstory/dao/CommentDAO.java
@@ -1,5 +1,9 @@
 package com.travelstory.dao;
 
-public interface CommentDAO {
+
+import com.travelstory.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentDAO extends JpaRepository<Comment, Long> {
 
 }
--- src/main/java/com/travelstory/services/AdminServiceImpl.java
+++ src/main/java/com/travelstory/services/AdminServiceImpl.java
@@ -1,50 +1,84 @@
 package com.travelstory.services;
 
-import com.travelstory.entity.User;
+import com.travelstory.dao.CommentDAO;
+import com.travelstory.dao.TravelStoryDAO;
+import com.travelstory.dao.UserDAO;
+import com.travelstory.entity.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AdminServiceImpl implements AdminService {
+
+    @Autowired
+    UserDAO userDAO;
+
+    @Autowired
+    User user;
+
+    @Autowired
+    CommentDAO commentDAO;
+
+    @Autowired
+    TravelStoryDAO travelStoryDAO;
+
     @Override
     public void addUser(User user) {
-
+        userDAO.save(user);
     }
 
     @Override
     public User getUserById(long id) {
-        return null;
-    }
-
-    @Override
-    public void editUser(long id) {
-
+        return userDAO.findById(id).get();
     }
 
     @Override
     public void deleteUser(long id) {
-
+        userDAO.deleteById(id);
     }
 
     @Override
-    public void blockUser(long id) {
-
+    public  void markAsDeleted(long id){
+        user = userDAO.findById(id).get();
+        user.setUserStatus(UserStatus.DELETED);
+        userDAO.save(user);
     }
 
     @Override
-    public void banUser(long id) {
+    public  void markAsActive(long id){
+        user = userDAO.findById(id).get();
+        user.setUserStatus(UserStatus.ACTIVE);
+        userDAO.save(user);
+    }
 
+    @Override
+    public  void markAsBanned(long id){
+        user = userDAO.findById(id).get();
+        user.setUserStatus(UserStatus.BANNED);
+        userDAO.save(user);
     }
 
     @Override
     public void setAdminStatus(long id) {
+        user = userDAO.findById(id).get();
+        user.setUserRole(UserRole.ROLE_ADMIN);
+        userDAO.save(user);
+    }
 
+    @Override
+    public void setUserStatus(long id) {
+        user = userDAO.findById(id).get();
+        user.setUserRole(UserRole.ROLE_USER);
+        userDAO.save(user);
     }
 
     @Override
     public void deleteComment(long id) {
-
+        commentDAO.deleteById(id);
     }
 
     @Override
     public void deleteTravelStory(long id) {
-
+        travelStoryDAO.deleteById(id);
     }
 }
