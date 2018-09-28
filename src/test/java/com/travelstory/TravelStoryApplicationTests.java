5670a3c3f4a6eaafe02c750cca7e5f018f414e00
Zhuravel Stas <zhuravelstas@gmail.com>
2018-09-28 16:01:43 +0300

    fix gitignore

--- src/test/java/com/travelstory/TravelStoryApplicationTests.java
+++ src/test/java/com/travelstory/TravelStoryApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TravelStoryApplicationTests  {
+public class TravelStoryApplicationTests {
 
     @Test
     public void contextLoads() {
