b9d4054f7710f8c465e32a0fa43be97903ad0c65
Roma Shevchuk <shevromanvk@gmail.com>
2018-10-21 14:30:55 +0300

    update userDTO

--- src/main/java/com/travelstory/dto/UserDto.java
+++ src/main/java/com/travelstory/dto/UserDto.java
@@ -22,11 +22,4 @@ public class UserDTO {
     private List<Long> usersFollows;
     private List<TravelStory> travelStories;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
--- src/main/java/com/travelstory/dto/UserPicDTO.java
+++ src/main/java/com/travelstory/dto/UserPicDTO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 public class UserPicDTO {
 
     private Long id;
+
     private String pic;
 
 }
