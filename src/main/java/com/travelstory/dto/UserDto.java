7ebd45d98216a61c76d3a04a72fe595699844fd7
Oleh Krutiak <okrutiak@gmail.com>
2018-10-22 12:51:49 +0300

    conflict resolve

--- src/main/java/com/travelstory/dto/UserDto.java
+++ src/main/java/com/travelstory/dto/UserDto.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Data
-public class UserDto {
+public class UserDTO {
     private Long id;
     private String firstName;
     private String lastName;
