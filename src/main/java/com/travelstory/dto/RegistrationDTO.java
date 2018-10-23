458fd07e721288d698e7f19ee85dc4792452afd4
Roma Shevchuk <shevromanvk@gmail.com>
2018-10-23 20:40:17 +0300

    Update src/main/java/com/travelstory/dto/RegistrationDTO.java

--- src/main/java/com/travelstory/dto/RegistrationDTO.java
+++ src/main/java/com/travelstory/dto/RegistrationDTO.java
@@ -3,9 +3,11 @@ package com.travelstory.dto;
 import com.travelstory.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class RegistrationDTO {
     private String email;
     private String password;
