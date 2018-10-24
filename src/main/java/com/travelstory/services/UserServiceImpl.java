2e98f7cca43cfa36fd9922d2057305dfc3504a34
Roma Shevchuk <shevromanvk@gmail.com>
2018-10-24 14:40:40 +0300

    Update src/main/java/com/travelstory/services/UserServiceImpl.java

--- src/main/java/com/travelstory/services/UserServiceImpl.java
+++ src/main/java/com/travelstory/services/UserServiceImpl.java
@@ -19,8 +19,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
