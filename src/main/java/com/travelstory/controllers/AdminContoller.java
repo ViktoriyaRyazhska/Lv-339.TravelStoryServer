c14546791354a9fa5971ea09e2d0839a83b04f8c
mdzhura <32909428+mdzhura@users.noreply.github.com>
2018-11-05 20:51:04 +0200

    Fix checkstyle error

--- src/main/java/com/travelstory/controllers/AdminContoller.java
+++ src/main/java/com/travelstory/controllers/AdminContoller.java
@@ -2,7 +2,6 @@ package com.travelstory.controllers;
 
 import com.travelstory.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
